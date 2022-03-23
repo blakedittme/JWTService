8ffcc91255b573c0675f4729cd3e7cb3caa58b41
Blake Dittmer <69608491+blakedittme@users.noreply.github.com>
2022-03-23 18:36:03 -0500

    initial commit

---
+++ AuthenticationService/Models/JWTContainerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthenticationService.Models
+{
+    public class JWTContainerModel : IAuthContainerModel
+    {
+        public int ExpireMinutes { get; set; } = 10080;
+        public string SecretKey { get; set; } = "TW9zaGVFcmV6UHJpdmF0ZUtleQ=="; //This should be a secure string on a server
+        public string SecurityAlgorithm { get; set; } = SecurityAlgorithms.HmacSha256Signature;
+        public Claim[] Claims { get; set; }
+    }
+}
